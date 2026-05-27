2630af21fea843fe81e6c34fe12fe24bfd58f889
muneebfr-fr <muneebkashif267@gmail.com>
2026-05-28 02:09:45 +0500

    feat: swap to transparent PNG logo across entire site

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const WA_QUOTE = "https://wa.me/923001234567?text=Hello%20Levated%20Enter
 export const WA_BULK  = "https://wa.me/923001234567?text=Hello%20Levated%20Enterprises%2C%20I%20would%20like%20to%20discuss%20a%20bulk%20procurement%20requirement.";
 export const WA_CITY  = "https://wa.me/923001234567?text=Hello%2C%20I%20need%20delivery%20to%20a%20specific%20city%20in%20Pakistan.";
 export const WA_BASE  = "https://wa.me/923001234567";
-export const LOGO     = "/logo.jpg";
+export const LOGO     = "/logo.png";
