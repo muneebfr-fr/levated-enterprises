1cd3ab6e2df2f371e4c03cc696cb8a2e2fe6be65
muneebfr-fr <muneebkashif267@gmail.com>
2026-05-28 15:07:51 +0500

    chore: update WhatsApp number to +92 333 3122898

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const WA_QUOTE = "https://wa.me/923001234567?text=Hello%20Levated%20Enterprises%2C%20I%20would%20like%20to%20request%20a%20procurement%20quote.";
-export const WA_BULK  = "https://wa.me/923001234567?text=Hello%20Levated%20Enterprises%2C%20I%20would%20like%20to%20discuss%20a%20bulk%20procurement%20requirement.";
-export const WA_CITY  = "https://wa.me/923001234567?text=Hello%2C%20I%20need%20delivery%20to%20a%20specific%20city%20in%20Pakistan.";
-export const WA_BASE  = "https://wa.me/923001234567";
+export const WA_QUOTE = "https://wa.me/923333122898?text=Hello%20Levated%20Enterprises%2C%20I%20would%20like%20to%20request%20a%20procurement%20quote.";
+export const WA_BULK  = "https://wa.me/923333122898?text=Hello%20Levated%20Enterprises%2C%20I%20would%20like%20to%20discuss%20a%20bulk%20procurement%20requirement.";
+export const WA_CITY  = "https://wa.me/923333122898?text=Hello%2C%20I%20need%20delivery%20to%20a%20specific%20city%20in%20Pakistan.";
+export const WA_BASE  = "https://wa.me/923333122898";
 export const LOGO     = "/logo.png";
